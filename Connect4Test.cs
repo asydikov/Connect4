f49ec122a577f1e2142a8d2cb7bc3f295e674356
Aziz Sydikov <asydikov@gmail.com>
2020-03-17 23:37:54 +0000

    Update Connect4Test.cs

--- Connect4Test.cs
+++ Connect4Test.cs
@@ -17,7 +17,7 @@ namespace Connect4
                                         -------
                                         -------
                                         -------
-                                        x-xx-xx
+                                        x-x--xx
                                        ";
 
         private static string case2 = @"-------
@@ -55,7 +55,7 @@ namespace Connect4
         [Test]
         public static void Test()
         {
-            Assert.AreEqual(new int[] { 2, 5 }, Connect4.Start(case1), "Case 1");
+            Assert.AreEqual(new int[] {  }, Connect4.Start(case1), "Case 1");
             Assert.AreEqual(new int[] { 2, 4, 5, 7 }, Connect4.Start(case2), "Case 2");
             Assert.AreEqual(new int[] { 4, 6 }, Connect4.Start(case3), "Case 3");
             Assert.AreEqual(new int[] { 4 }, Connect4.Start(case4), "Case 4");
